73a45823bb905380ccaa8a0b012abd7a07f1c485
Plswearpants <94640411+Plswearpants@users.noreply.github.com>
2024-05-30 14:32:07 -0700

    Delete projectLattices.m

--- projectLattices.m
+++
@@ -1,9 +0,0 @@
-function [projection] = projectLattices(lattice, direction, n)
-    if nargin < 4
-        % Project the 3D lattice onto the plane without layer filtering
-        projection = projectLatticeToPlane(lattice, direction);
-    else
-        % Project the 3D lattice onto the plane with layer filtering
-        projection = projectLatticeToPlane(lattice, direction, n);
-    end
-end
